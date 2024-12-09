f94a0e492ac2c6ab704f290cd96bd10b0b9052dd
Muhammad Amsal <muhammadamsal520@gmail.com>
2024-12-09 23:58:49 +0500

    img position change

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -11,7 +11,7 @@ export default function Hero() {
         <div className="font-medium width-[1440px] height-[900px] " ><h1 className="text-5xl leading-tight" >Rocket Single <br/>Seater</h1>
         <h3 className="mt-4 border-b-2 border-black w-1/3 text-xl" >Shop Now</h3>
         </div>
-        <div><Image  src={hero} width={853} height={1000}   alt="HeroImg" /></div>
+        <div><Image  src={hero} width={853} height={1000} className="scale-x-[-1]"  alt="HeroImg" /></div>
     </div>
     
     </section>
