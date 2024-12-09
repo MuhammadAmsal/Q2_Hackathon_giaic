b0577a7b88edb1d1a7965f93776c0a09e4438235
Muhammad Amsal <muhammadamsal520@gmail.com>
2024-12-09 11:21:45 +0500

    remove commas

--- src/app/components/ProductDescription.tsx
+++ src/app/components/ProductDescription.tsx
@@ -15,13 +15,13 @@ export default function ProductDescription() {
         <div className="flex flex-col pb-16" >
           <p className="text-sm" >
             Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text ever
+            industry. Lorem Ipsum has been the industry standard dummy text ever
             since the 1500s, when an unknown printer took a galley of type and
             scrambled it to make a type specimen book
           </p>
           <p className="text-sm" >
             Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text ever
+            industry. Lorem Ipsum has been the industry standard dummy text ever
             since the 1500s, when an unknown printer took a galley of type and
             scrambled it to make a type specimen book
           </p>
