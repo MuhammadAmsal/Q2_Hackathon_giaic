b3e65de40db093561e429ccefcea22b7f5d09e84
Muhammad Amsal <muhammadamsal520@gmail.com>
2025-01-25 23:20:29 +0500

    Update data-migration.mjs

--- scripts/data-migration.mjs
+++ scripts/data-migration.mjs
@@ -3,11 +3,11 @@ import fetch from 'node-fetch';
 
 // Initialize Sanity client
 const client = createClient({
-  projectId: "qc9ei68n",
+  projectId: "projectId",
   dataset: "production",
-  useCdn: false, // Set to true if you want faster reads
+  useCdn: false,
   apiVersion: '2025-01-13',
-  token: "skz24mJdbUITH1q93afhfAWH9J80Evks7IFIO03DjYJ3TbGMYDtCkEosI5P718fVD3Lo7bzT8LzzIfU7ITTni7bgwfArJ5LXsLa2XY1XkaYdb5ZY58sbNpbrIHfputGeUgU4d32Q6vdR36gyVKT4q0bZUvqxcQ93hNhVPN2ZwSxiS7R8ICQr", // Replace with your Sanity token
+  token: "token",
 });
 
 // Function to upload an image to Sanity
